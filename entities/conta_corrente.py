75eb8abc52fb204200e6a9c27e0b847f7065e521
Rodrigo L. <cabelin2104@gmail.com>
2025-10-17 18:50:40 -0300

    Ajustes no limite e limete de saques, criação do método saque para uma melhor delegação e controle sobre o saque.

--- entities/conta_corrente.py
+++ entities/conta_corrente.py
@@ -1,7 +1,25 @@
 from .conta import Conta
 
 class Conta_Corrente(Conta):
-    def __init__(self, numero, agencia, cliente, historico):
-        super().__init__(numero, agencia, cliente, historico)
-        self._limite = 500.0
-        self._limite_saques = 3
+    def __init__(self, numero,cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self._limite = limite
+        self._limite_saques = limite_saques
+
+    def sacar(self, valor):
+        from .transacao import Saque
+
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao['tipo'] == Saque.__name__]
+        )
+        
+        if numero_saques >= self._limite_saques:
+            print('\nConta Chegou ao Limite De Saques Diários')
+
+        elif valor > self._limite:
+            print('\nValor Inserido Excede o Limite De Saque Da Conta')
+
+        else:
+            return super().sacar(valor)
+        
+        return False
